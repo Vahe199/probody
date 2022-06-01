0c0b8b5de1507a8d5ac597fa8eeb508d7e3f6a1a
mike <nmish2005@gmail.com>
2022-06-01 23:36:23 +0300

    added tags

---
+++ src/components/kit/Tag.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import css from '../../styles/kit/tag.module.scss'
+import PropTypes from "prop-types"
+import Icon from "./Icon.jsx"
+import {cnb} from "cnbuilder";
+
+export default class Tag extends React.Component {
+    static propTypes = {
+        icon: PropTypes.string,
+        label: PropTypes.string,
+        enabled: PropTypes.bool,
+    }
+
+    static defaultProps = {
+        enabled: true,
+    }
+
+    render() {
+        return <div className={cnb(css.root, this.props.enabled ? css.enabled : css.disabled)}>
+            {this.props.icon && <Icon name={this.props.icon}/>}
+            <span style={{marginLeft: this.props.icon ? 8 : 0}}>{this.props.label}</span>
+        </div>
+    }
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import {withRouter} from "next/router.js"
 import {GlobalContext} from "../contexts/Global.js"
-import RadioGroup from "../components/kit/Form/RadioGroup.jsx";
+import Tag from "../components/kit/Tag.jsx";
 
 class Home extends React.Component {
   render() {
@@ -12,11 +12,7 @@ class Home extends React.Component {
         <div>
             <p className="subtitle additional-text">{t('greet')} 👋</p>
           <h1>Мы подобрали массажные салоны в Алматы</h1>
-          <div style={{maxWidth: 400}}><RadioGroup columnView={true} name={'Подойдет'} options={[
-            {label: 'Мужчинам', value: 'men'},
-            {label: 'Женщинам', value: 'women'},
-            {label: 'Парам', value: 'couples'},
-          ]} /></div>
+          <div style={{maxWidth: 400}}><Tag label={'Классический'} enabled={false} icon={'man'} /></div>
             <p className="subtitle additional-text">Привет 👋</p>
             <h1>Мы подобрали массажные салоны в Алматы</h1>
             <p className="subtitle additional-text">Привет 👋</p>
