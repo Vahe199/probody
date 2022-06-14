1df45ae8c191f24ad6c86cce1d8d9c8d85d54d95
mike <nmish2005@gmail.com>
2022-06-14 10:12:22 +0300

    modified search back-end

--- src/v1/search/index.js
+++ src/v1/search/index.js
@@ -46,7 +46,8 @@ router.post('/worker', async (req, res) => {
 
     try {
         parsedPN = parsePhoneNumber(req.body.query, process.env.PHONE_REGION)
-    } catch (e) {}
+    } catch (e) {
+    }
 
     // if (!req.body.query || req.body.query.length < 3) {
     //     return res.status(422).json({
@@ -66,16 +67,16 @@ router.post('/worker', async (req, res) => {
         req.query.limit = 0
     }
 
+    if (req.body.filters) {
+        for (let filterName in req.body.filters) {
+            req.body.query += `@${filterName}:{${req.body.filters[filterName]}} `
+        }
+    }
+
     if (parsedPN && parsedPN.isValid()) {
         req.body.query = '@phone:{' + parsedPN.number.replace('+', '') + '}'
     } else {
-        // req.body.query += '*'
-    }
-
-    if (req.body.filters) {
-        for (let filterName in req.body.filters) {
-            req.body.query += ` @${filterName}:{${req.body.filters[filterName]}}`
-        }
+        req.body.query += '*'
     }
 
     res.json(await Search.findWorker(req.body.query, req.query.hasOwnProperty('mapView'), req.query.limit, (req.query.page - 1) * req.query.limit))
