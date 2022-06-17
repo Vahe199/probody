c1c92173a421c3b4abe508829f729db41fe92597
mike <nmish2005@gmail.com>
2022-06-17 15:39:33 +0300

    added mobile search

--- src/helpers/Search.js
+++ src/helpers/Search.js
@@ -150,6 +150,24 @@ export default class Search {
                 workerQuery.populate('region', 'name').projection('location name slug isVerified photos address social programs description phone messengers region')
             }
 
+            console.log([{
+                $match: {
+                    target: {
+                        $in: searchResultsIds.map(id => new mongoose.Types.ObjectId(id))
+                    }
+                }
+            }, {
+                $group: {
+                    _id: '$target',
+                    avg: {
+                        $avg: '$avg'
+                    },
+                    count: {
+                        $count: {}
+                    }
+                }
+            }])
+
             return {
                 pageCount: Math.ceil(searchResults[0] / limit), //searchResults[0] is total count
                 results: await workerQuery,
