2f84abdbb3d86b220df88a05f3575f9629ec5f28
mike <nmish2005@gmail.com>
2022-07-01 13:18:35 +0300

    fixed scroll

--- src/helpers/Search.js
+++ src/helpers/Search.js
@@ -225,11 +225,19 @@ export default class Search {
                 )
             }
 
+            let regionFilter = ''
+
+            if (queryString.match(/@region:[\u0400-\u04FF]+/g)) {
+                regionFilter = queryString.match(/@region:[\u0400-\u04FF]+/g)[0].toLowerCase()
+            } else {
+                regionFilter = 'астана'
+            }
+
             return {
                 pageCount: Math.ceil(searchResults[0] / limit), //searchResults[0] is total count
                 results: await Worker.aggregate(workerAggregation),
                 count: searchResults[0],
-                workerLocations: queryString.match(/@region:[\u0400-\u04FF]+/g) ? (await RedisHelper.ftSearchRaw('idx:worker', queryString.match(/@region:[\u0400-\u04FF]+/g)[0].toLowerCase(), 'RETURN', '1', 'location')).splice(1).filter((val, i) => (i + 1) % 2 === 0).map(val => val[1].split(',').map(Number)) : [],
+                workerLocations: queryString.match(/@region:[\u0400-\u04FF]+/g) ? (await RedisHelper.ftSearchRaw('idx:worker', regionFilter, 'RETURN', '1', 'location')).splice(1).filter((val, i) => (i + 1) % 2 === 0).map(val => val[1].split(',').map(Number)) : [],
                 reviews: await Review.aggregate([{
                     $match: {
                         target: {
