e0bd62bbc105d79064f077b30c923069f3716d39
mike <nmish2005@gmail.com>
2022-07-18 18:19:40 +0300

    added social block on mobile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -20,7 +20,7 @@ export default class Footer extends React.Component {
                 <div bp={'grid'}>
                     <div bp={'8@md 12@sm'}>
                         <div bp={'grid 4@md 6'} style={{gap: isMobile ? 24 : 32}}>
-                            <div><Link href={'/'}>{t('cities')}</Link></div>
+                            <div><Link href={'/cities'}>{t('cities')}</Link></div>
                             <div><Link href={'/contact'}>{t('contacts')}</Link></div>
                             <div><Link href={'/'}>{t('aboutProject')}</Link></div>
                             <div><Link href={'/'}>{t('visitors')}</Link></div>
