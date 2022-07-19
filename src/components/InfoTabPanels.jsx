921f670ba514ad6a2d07f920a1b46e07754696dc
mike <nmish2005@gmail.com>
2022-07-19 18:25:41 +0300

    added social block on mobile

--- src/components/InfoTabPanels.jsx
+++ src/components/InfoTabPanels.jsx
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 import InfoBlock from "./kit/InfoBlock";
 import css from '../styles/kit/infotp.module.scss'
 import Button from "./kit/Button.jsx";
+import {cnb} from "cnbuilder";
 
 export default class InfoTabPanels extends React.Component {
     static contextType = GlobalContext
@@ -33,8 +34,19 @@ export default class InfoTabPanels extends React.Component {
             activeTab = this.props.tabs[this.state.tabIndex]
 
         return isMobile ?
-            <div>
-                mobile
+            <div className={cnb(css['theme--' + theme])}>
+                <h1 className={'responsive-content'} style={{marginBottom: 16}}>{this.props.title}</h1>
+                <div className={css.invisibleScroll}>
+                    {this.props.tabs.map((tab, i) =>
+                        <div key={i} className={css.mobileTab}>
+                            <strong>{activeTab.shortTitle}</strong>
+                            <div className={css.mobilePic} style={{backgroundImage: `url('${activeTab.pic}')`}}>&nbsp;</div>
+                            <strong>{activeTab.title}</strong>
+                            <p style={{margin: '17px 0 48px 0'}}>{activeTab.text}</p>
+                            <Button onClick={this.props.onActionClick} size={'fill'}>{this.props.buttonText}</Button>
+                        </div>
+                    )}
+                </div>
             </div>
             : <div className={css['theme--' + theme]}>
                 <InfoBlock style={{padding: '40px 40px 60px 40px'}}>
@@ -42,7 +54,8 @@ export default class InfoTabPanels extends React.Component {
 
                     <div className={css.tabsHead}>
                         {this.props.tabs.map((tab, i) =>
-                            <span key={i} className={this.state.tabIndex === i ? css.active : ''} onClick={() => this.setState({tabIndex: i})}>{tab.shortTitle}</span>
+                            <span key={i} className={this.state.tabIndex === i ? css.active : ''}
+                                  onClick={() => this.setState({tabIndex: i})}>{tab.shortTitle}</span>
                         )}
                     </div>
 
@@ -52,7 +65,7 @@ export default class InfoTabPanels extends React.Component {
                         <div className={css.pic} style={{backgroundImage: `url('${activeTab.pic}')`}}>&nbsp;</div>
                         <div style={{maxWidth: 490}}>
                             <h1>{activeTab.title}</h1>
-                            <p style={{margin: '17px 0 48px 0'}}>{activeTab.text}</p>
+                            <p style={{margin: '17px 0 48px 0'}} className={css.text}>{activeTab.text}</p>
                             <Button onClick={this.props.onActionClick} size={'large'}>{this.props.buttonText}</Button>
                         </div>
                     </div>
